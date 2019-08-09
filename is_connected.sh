5c6c63d26cefd9eead3ac806bcec0d4bd2e85297
Ngoc Luong <baongock32@gmail.com>
2019-08-09 09:32:24 +0700

    Update config

--- is_connected.sh
+++ is_connected.sh
@@ -6,7 +6,7 @@ if [[ `nmcli -t device show wlp3s0 | grep "GENERAL.STATE"` == *"(connected)"* ]]
    if [[ `nmcli -t device show wlp3s0 | grep "GENERAL.STATE"` == *"(connected)"* ]];then
       echo "wifi connected:" `nmcli -t device show wlp3s0 | grep "GENERAL.CONNECTION" | cut -d":" -f2-` ""
    elif [[ `nmcli -t device show enp0s25 | grep "GENERAL.STATE"` == *"(connected)"* ]];then
-      echo "───────- Cable connected:" `nmcli -t device show enp0s25 | grep "GENERAL.CONNECTION" | cut -d":" -f2-` ""
+      echo "cable connected:" `nmcli -t device show enp0s25 | grep "GENERAL.CONNECTION" | cut -d":" -f2-` ""
    fi
 else 
    echo "not connected"
